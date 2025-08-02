6790309e7c89460c4990da7d7b66838b360d926d
Hussen <ha0959943@gmail.com>
2025-08-02 08:51:45 +0300

    profile avatar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
-import { Button } from "@/components/ui/button";
-import { CirclePlus } from "lucide-react";
-
 export default function Home() {
-  return (
-    <div className=" h-screen flex justify-center items-center text-custom-color">
-      <Button>
-        <CirclePlus />
-        Add
-      </Button>
-    </div>
-  );
+  return <div></div>;
 }
